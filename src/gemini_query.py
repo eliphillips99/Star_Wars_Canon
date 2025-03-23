1e9395dcd8b27e5307da776946d2112412b47cb3
Eli <eli.g.phillips99@gmail.com>
2025-03-22 23:57:19 -0400

    Currently generating summaries for each media item, yet to do a fully successful test run.

--- src/gemini_query.py
+++ src/gemini_query.py
@@ -3,25 +3,46 @@ import google
 from google import genai
 import gspread
 import pandas as pd
+import time
 
-gc = gspread.api_key("AIzaSyD9gJandrwDO-FpjGIZxtt9I0srgRqsJ74")
+with open("C:\\Users\\eligp\\OneDrive\\Documents\\Coding Projects\\Star_Wars_Canon\\ref\\gspread_API_key.txt","r") as f:
+    gspread_key = f.read()
+gc = gspread.api_key(gspread_key)
 
 # Open a sheet from a spreadsheet in one go
 sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1PyWmeM1nwzQV6anIfTp9nre5Wg6-eTfFyYBk8R9z4cY/edit?gid=0#gid=0")
 Timeline = sh.worksheet("Timeline")
 data = Timeline.get_all_records()
 df = pd.DataFrame.from_records(data)
-print(df)
 
 
-client = genai.Client(api_key="AIzaSyCu9ptrc2qRwNEeKl1hzEkD-5FtSBMTPSQ")
+with open("C:\\Users\\eligp\\OneDrive\\Documents\\Coding Projects\\Star_Wars_Canon\\ref\\gemini_API_key.txt","r") as f:
+    gem_key = f.read()
+client = genai.Client(api_key=gem_key)
 
 
+media_name = 'blank'
+prompt = 'Analyze the following Star Wars media: ' + str(media_name) + '.' \
+'         Provide a detailed summary of the plot, highlighting key events and character arcs.' \
+'         List the major characters present in this media.' \
+'         Identify and describe the top 3 major themes explored.' \
+'         Describe the overall tone and atmosphere of the media using 3-5 key words .'
 
-response = client.models.generate_content(model="gemini-2.0-flash", contents="give me a synopsis of revenge of the sith")
+response_list = []
+for i in range(len(df)):
+    media_name = df["Name"][i]
+    response = client.models.generate_content(model="gemini-2.0-flash", contents=prompt)
+    response_list.append(response.text)
+    print(media_name)
+    
+    if i % 10 == 0:
+        print("Sleeping for 61 seconds")
+        time.sleep(61)
+        
 
-print(response.text) 
 
+df['Full Summary'] = response_list
+df.to_csv("C:\\Users\\eligp\\OneDrive\\Documents\\Coding Projects\\Star_Wars_Canon\\data\\Timeline.csv", index=False)
 '''
 # Currently using Gemini 2.0 Flash
 def query_gemini_api(endpoint, params=None, api_key=None):
