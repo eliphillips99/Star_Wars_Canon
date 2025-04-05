e9edcec405ad613699f2e0cbb3333c4a8721bedb
Eli <eli.g.phillips99@gmail.com>
2025-04-05 09:23:52 -0400

    Baseline Commit

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ def main():
     df = load_google_sheet_data(API_KEYS["gspread"])
 
     # Specify the number of items to process
-    items_to_process = 25  # Change this value to process a different number of entries
+    items_to_process = 20  # Change this value to process a different number of entries
 
     # Process the specified number of entries, remove items_to_process to process all
     # results_df = process_all_entries(df, API_KEYS["tmdb"], API_KEYS["gemini"])
